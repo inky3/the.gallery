f8c6ff5cdd284df3ea7ffe93b1550b7f7420f9c8
unknown <Boom05@windowslive.com>
2025-08-30 20:06:33 +0700

    hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Add this line to specify static output
+  output: 'static',
+  // Replace <YOUR_REPOSITORY_NAME> with your actual repository name
+  base: '/<the.gallery>/',
+});
