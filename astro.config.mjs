3bc17aa13d203d8cdbc04090081be7f6470ce949
unknown <Boom05@windowslive.com>
2025-10-16 07:17:23 +0700

    Porsche Image Update fix config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default ({
+export default defineConfig({
   site: 'https://1nky3.github.io',
   base: '/the.gallery/', // or '/the.gallery/' for the other repo
 });
