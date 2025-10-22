f58b26454d2709e776ded820ead577a04621ce2f
unknown <Boom05@windowslive.com>
2025-10-22 23:28:29 +0700

    Porject update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://1nky3.github.io',
-  base: '//', // or '/the.gallery/' for the other repo
+  site: 'https://1nky3.github.io/the.gallery',
+  base: '/the.gallery/',
 });
