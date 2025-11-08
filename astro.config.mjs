6c98246d90dd6081ee1e7c6a2fc32736cc3f8839
unknown <Boom05@windowslive.com>
2025-11-08 11:14:43 +0700

    redesign whole website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: 'https://1nky3.github.io/the.gallery',
-  base: '/the.gallery/',
+  base: isProd ? '/the.gallery/' : '/', // ✅ dev works at /
 });
